294f0456f8c81eee3f65397181af135e3ac00403
davidfu1126 <davidwuw0811@gmail.com>
2026-05-06 23:06:41 +0800

    Fix video scene fade timing

--- outlet-video-demo/src/Video.tsx
+++ outlet-video-demo/src/Video.tsx
@@ -7,10 +7,13 @@ import {Scene04Interior} from './scenes/Scene04Interior';
 import {Scene05Ending} from './scenes/Scene05Ending';
 
 const TRANSITION_FRAMES = 18;
+const SCENE_FRAMES = 90;
 
-const Fade: React.FC<{start: number; children: React.ReactNode}> = ({start, children}) => {
+const FadeIn: React.FC<{children: React.ReactNode}> = ({children}) => {
+  // IMPORTANT: useCurrentFrame() is local to this Sequence.
+  // Therefore fade timing must start at 0, not at the global timeline frame.
   const frame = useCurrentFrame();
-  const opacity = interpolate(frame, [start, start + TRANSITION_FRAMES], [0, 1], {
+  const opacity = interpolate(frame, [0, TRANSITION_FRAMES], [0, 1], {
     extrapolateLeft: 'clamp',
     extrapolateRight: 'clamp',
   });
@@ -21,32 +24,32 @@ const Fade: React.FC<{start: number; children: React.ReactNode}> = ({start, chil
 export const OutletVideo: React.FC = () => {
   return (
     <AbsoluteFill style={{backgroundColor: '#050505'}}>
-      <Sequence from={0} durationInFrames={90}>
+      <Sequence from={0} durationInFrames={SCENE_FRAMES}>
         <Scene01Current />
       </Sequence>
 
-      <Sequence from={90 - TRANSITION_FRAMES} durationInFrames={90 + TRANSITION_FRAMES}>
-        <Fade start={90 - TRANSITION_FRAMES}>
+      <Sequence from={SCENE_FRAMES - TRANSITION_FRAMES} durationInFrames={SCENE_FRAMES + TRANSITION_FRAMES}>
+        <FadeIn>
           <Scene02Overall />
-        </Fade>
+        </FadeIn>
       </Sequence>
 
-      <Sequence from={180 - TRANSITION_FRAMES} durationInFrames={90 + TRANSITION_FRAMES}>
-        <Fade start={180 - TRANSITION_FRAMES}>
+      <Sequence from={SCENE_FRAMES * 2 - TRANSITION_FRAMES} durationInFrames={SCENE_FRAMES + TRANSITION_FRAMES}>
+        <FadeIn>
           <Scene03Storefront />
-        </Fade>
+        </FadeIn>
       </Sequence>
 
-      <Sequence from={270 - TRANSITION_FRAMES} durationInFrames={120 + TRANSITION_FRAMES}>
-        <Fade start={270 - TRANSITION_FRAMES}>
+      <Sequence from={SCENE_FRAMES * 3 - TRANSITION_FRAMES} durationInFrames={SCENE_FRAMES + TRANSITION_FRAMES}>
+        <FadeIn>
           <Scene04Interior />
-        </Fade>
+        </FadeIn>
       </Sequence>
 
-      <Sequence from={360 - TRANSITION_FRAMES} durationInFrames={90 + TRANSITION_FRAMES}>
-        <Fade start={360 - TRANSITION_FRAMES}>
+      <Sequence from={SCENE_FRAMES * 4 - TRANSITION_FRAMES} durationInFrames={SCENE_FRAMES + TRANSITION_FRAMES}>
+        <FadeIn>
           <Scene05Ending />
-        </Fade>
+        </FadeIn>
       </Sequence>
     </AbsoluteFill>
   );
